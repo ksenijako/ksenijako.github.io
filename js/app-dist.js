2b9bdc0a8a793119786b59a77f834bf43495fd5c
Ksenia Kolyadina <ksenija.koladina@gmail.com>
2018-11-29 22:49:05 +0200

    third homework - slider and menu

---
+++ js/app-dist.js
@@ -0,0 +1,1 @@
+document.querySelector("#hamburger").addEventListener("click",function(){document.querySelector("#nav").classList.toggle("nav_full")});
---
+++ js/app.js
@@ -0,0 +1,4 @@
+document.querySelector('#hamburger').addEventListener('click', function () {
+  var nav = document.querySelector('#nav');
+  nav.classList.toggle('nav_full');
+});
